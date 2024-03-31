a009e916411011d7da7386d7b08859ce0c11773d
jakepears <jake.pearson.1694@gmail.com>
2024-03-31 13:22:10 -0700

    working product :D

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# Professional README Generator Starter Code
+<!-- @format -->
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+# readmeMaker
+
+## LicenseMIT![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)https://opensource.org/licenses/MIT
+
+## Description
+
+This project uses javascript and node.js to create a commandline tool that helps create a professional style readme.md
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+clone the project, then run `node index.js`
+
+## Usage
+
+This tool excels at helping users create a professional readme without the need of learning the markdown language
+
+## License
+
+MIT
+
+## Contributing
+
+n/a
+
+## Tests
+
+n/a
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at jake.pearson.1694@gmail.com. You can find more of my work at [jakepears](https://github.com/jakepears).
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at jake.pearson.1694@gmail.com. You can find more of my work at [jakepears](https://github.com/jakepears).
--- index.js
+++ index.js
@@ -1,65 +1,82 @@
 /** @format */
-Include the necessary packages, such as inquirer for prompting the user and fs for writing the README file.
 // Create an array of questions for user input using inquirer. Each question should have a type (e.g., input, list), name, and message property. Include questions for the project title, description, installation instructions, usage information, contribution guidelines, test instructions, license choice, GitHub username, and email address.
 // Implement the writeToFile function to write the generated README content to a file. Use the fs.writeFile method to write the content to a file with the specified fileName.
 // Implement the init function to initialize the application. Use inquirer.prompt to prompt the user with the questions array. Once the user provides the answers, pass the answers object to the generateMarkdown function to generate the README content. Finally, use the writeToFile function to write the generated content to a file.
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please provide a description of your project:',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please provide installation instructions:',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please provide usage information:',
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Please provide contribution guidelines:',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Please provide test instructions:',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Please choose a license for your project:',
-        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
-    },
-    {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub username?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-    },
+	{
+		type: 'input',
+		name: 'title',
+		message: 'What is the title of your project?',
+	},
+	{
+		type: 'input',
+		name: 'description',
+		message: 'Please provide a description of your project:',
+	},
+	{
+		type: 'input',
+		name: 'installation',
+		message: 'Please provide installation instructions:',
+	},
+	{
+		type: 'input',
+		name: 'usage',
+		message: 'Please provide usage information:',
+	},
+	{
+		type: 'input',
+		name: 'contributing',
+		message: 'Please provide contribution guidelines:',
+	},
+	{
+		type: 'input',
+		name: 'tests',
+		message: 'Please provide test instructions:',
+	},
+	{
+		type: 'list',
+		name: 'license',
+		message: 'Please choose a license for your project:',
+		choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+	},
+	{
+		type: 'input',
+		name: 'username',
+		message: 'What is your GitHub username?',
+	},
+	{
+		type: 'input',
+		name: 'email',
+		message: 'What is your email address?',
+	},
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+	fs.writeFile(fileName, data, (err) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('README.md file generated successfully!');
+		}
+	});
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+	inquirer
+		.prompt(questions)
+		.then((answers) => {
+			const markdown = generateMarkdown(answers);
+			writeToFile('README.md', markdown);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
 
-// Function call to initialize app
 init();
